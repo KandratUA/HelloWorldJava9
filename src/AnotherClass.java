1c13500bfddc76d1211deb3d1e433ad04f4761e7
KandratUA <kondratinskyi@gmail.com>
2022-10-04 20:33:12 +0300

    Initial commit

---
+++ src/AnotherClass.java
@@ -0,0 +1,11 @@
+public class AnotherClass {
+
+    static int age = 28;
+
+    static String name = "Dmytro";
+
+    public static void printNameAge() {
+        System.out.println("My name is " + name + " i am " + age);
+        System.out.println("In the comment");
+    }
+}
